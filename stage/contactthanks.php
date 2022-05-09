dec5e5d3b7e7445379a626e018daea8a2c3a012c
bobomatic1807 <104908120+bobomatic1807@users.noreply.github.com>
2022-05-09 15:20:34 -0500

    Add files via upload

---
+++ stage/contactthanks.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Contact Form</title>
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+		
+		
+			
+			
+		<br /><br />
+		
+			
+		<h1>Your message has been sent!</h1><br />
+		
+		<p><a href="/stage/index.html">Back to Home Page</a></p>
+	
+	</div>
+	
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	
+
+</body>
+
+</html>
